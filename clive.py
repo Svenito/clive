776d5a20539a4c043b9c30cd581a5d6d099e2ef6
Sven Steinbauer <sven@themill.com>
2016-03-22 15:15:50 +0000

    Add on_connct method

--- clive.py
+++ clive.py
@@ -19,6 +19,10 @@ class Clive(object):
         self.quitting = False
         signal.signal(signal.SIGINT, self.signal_handler)
 
+        self.title = ''
+        self.description = ''
+        self.state = ''
+
     def signal_handler(self, signal, frame):
         """Handle CTRL+C to stop reconnection"""
         self.quitting = True
@@ -50,21 +54,34 @@ class Clive(object):
                 return
             self.print_item(data)
 
+    def on_error(self, socket, error):
+        print ('Socket error: ', error)
+        self.quitting = True
+        sys.exit(0)
+
     def on_close(self, socket):
         """Socket expired, fetch a new one if we're not quitting
         Unfortunately the websockets expire after a while"""
         if not self.quitting:
-            self.init_socket()
+            self.run()
+
+    def on_open(self, socket):
+        print ('Connected. Press Ctrl+c twice to quit.\n')
+        print (colorama.Style.BRIGHT + self.title)
+        print (self.description + colorama.Style.RESET_ALL)
+        print ('Feed status is: %s' % self.state)
+
 
     def run(self, debug=False):
         """Get the websocket URL and connect to it and start the process"""
-        print 'Connecting to live feed... ',
+        print ('Connecting to live feed... ')
         socket_url = self.socket_url()
 
         websocket.enableTrace(debug)
         ws = websocket.WebSocketApp(socket_url, on_message=self.on_message,
-                                    on_close=self.on_close)
-        print 'Connected.\n'
+                                    on_close=self.on_close,
+                                    on_error=self.on_error,
+                                    on_open=self.on_open)
         ws.run_forever()
 
     def socket_url(self):
@@ -72,8 +89,21 @@ class Clive(object):
         headers = {'User-Agent': self.user_agent}
         r = requests.get('http://www.reddit.com/live/%s/about.json' %
                          self.feed_name, headers=headers)
-        data = r.json()
-        return unescape(data['data']['websocket_url'])
+
+        if r.status_code != 200:
+            sys.exit(0)
+
+        try:
+            about = r.json()
+        except ValueError:
+            print ('Failed to get websocket address for feed')
+            sys.exit(1)
+
+        data = about['data']
+        self.title = data['title']
+        self.description = data['description']
+        self.state = data['state']
+        return unescape(data['websocket_url'])
 
 
 if __name__ == '__main__':
@@ -84,7 +114,7 @@ if __name__ == '__main__':
     in this case: wmk50bsm9vt3"""
 
     if len(sys.argv) < 2:
-        print 'You need to provide the name of the feed. Eg: wmk50bsm9vt3'
+        print ('You need to provide the name of the feed. Eg: wmk50bsm9vt3')
         sys.exit(0)
 
     feed = sys.argv[1]= 'wmk50bsm9vt3'
